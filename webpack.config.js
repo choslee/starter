b366e4372d268f27e57865715b38298bf6942ddc
choslee <zivanovic.dragoljub@gmail.com>
2017-11-27 09:51:50 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require ('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        indexJS: "./src/js/index.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist")   // Putanja fajla koji se generiše
+
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {loader: 'css-loader', options: {sourceMap: true}},     // translates CSS into CommonJS
+                        {loader: 'postcss-loader', options: {sourceMap: true}}, // postcss plugin loader
+                        {loader: 'sass-loader', options: {sourceMap: true}}     // compiles Sass to CSS
+                    ]
+                })
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: { limit: 40000 } // Convert images < 40k to base64 strings
+                    },
+                    {   loader: 'image-webpack-loader',}
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/views/index.html',
+            chunks: ['indexJS']
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new BrowserSyncPlugin({
+                host: 'localhost',
+                port: 3000,                       // url adresa za vreme developmenta http://localhost:3000/
+                proxy: 'http://localhost:8080/'   // proxy endpoint WebpackDev Servera  na http://localhost:8080
+            },
+            {
+                reload: false                     // Sprečava da BrowserSync reloaduje stranu, da bi to uradio WebpackDev Server
+            }
+        )
+    ]
+};
